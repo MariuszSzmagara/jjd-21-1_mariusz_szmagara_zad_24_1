eb438d96be482aa3c71b65a1308062c73a33f88a
MariuszSzmagara <mariusz.szmagara@gmail.com>
2021-04-17 18:49:54 +0200

    Initial commit

---
+++ src/main/java/pl/javastart/Jjd211MariuszSzmagaraZad241Application.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jjd211MariuszSzmagaraZad241Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jjd211MariuszSzmagaraZad241Application.class, args);
+    }
+
+}
